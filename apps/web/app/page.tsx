c88491e875d3c0ba2f2c53107a61cdddbc65fbe2
송성희 <thdtjdgml415@naver.com>
2025-08-12 23:23:15 +0900

    feat: integrate Convex for user management and add schema definition

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,25 @@
+"use client";
+
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 2)}</p>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p className="text-2xl font-bold">Hello web</p>
+      <Button
+        onClick={() => {
+          addUser({ name: "New User" });
+        }}
+      >
+        add
+      </Button>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   );
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
+"use client";
+
+import { api } from "@workspace/backend/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  console.log(users);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello widget</h1>
-        <Button size="sm">Button</Button>
-        {add(2, 2)}
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p className="text-2xl font-bold">Hello widget</p>
+      <div className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
   );
