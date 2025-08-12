9793429f15b607549552e3a95dde66f469fc2a32
송성희 <thdtjdgml415@naver.com>
2025-08-12 21:00:50 +0900

    feat : setup turbo repo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -5,9 +5,9 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello web</h1>
+        <h1 className="text-2xl font-bold">Hello widget</h1>
         <Button size="sm">Button</Button>
-        <p>{add(2, 2)}</p>
+        {add(2, 2)}
       </div>
     </div>
   );
